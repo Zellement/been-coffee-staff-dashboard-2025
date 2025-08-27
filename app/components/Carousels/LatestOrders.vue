f67a9121cf33579b84f3023dc9a83df931e5eb1b
Dan Farrow <dan@zellement.com>
2025-08-27 10:28:25 +0100

    continue

--- app/components/Carousels/LatestOrders.vue
+++ app/components/Carousels/LatestOrders.vue
@@ -1,77 +1,65 @@
 <template>
-    <div v-if="activeLocationId" class="">
-        <h2 class="uppercase">Latest orders</h2>
-        <div class="flex gap-4">
-            <div v-if="hasSortedRoutineTasks" class="min-w-0 flex-1">
+    <div class="p-default">
+        <h2 class="uc-text uc-text--xs">Latest orders</h2>
+        <div v-if="dataFetched" class="flex gap-4">
+            <div v-if="orders" class="min-w-0 flex-1">
                 <u-carousel
-                    v-if="sortedRoutineTasks"
+                    v-if="orders"
                     v-slot="{ item }"
-                    :items="sortedRoutineTasks"
+                    :items="orders"
                     auto-height
                     :ui="{ item: 'basis-48' }"
                 >
-                    {{ item.fields.details }}
+                    <pre>{{ item?.fields }}</pre>
                 </u-carousel>
             </div>
+        </div>
+        <div v-else class="flex w-full gap-4 overflow-hidden">
+            <u-skeleton class="h-17 w-17 shrink-0 rounded-full" />
+            <u-skeleton
+                v-for="i in 6"
+                :key="i"
+                class="h-17 shrink-0 basis-48"
+            />
         </div>
     </div>
 </template>
 
 <script setup lang="ts">
-const ordersStore = useOrdersStore()
+const locationsStore = useLocationsStore()
 
-// const { fetchDailyTaskInstances } = useTasksUtils()
-
-const today = new Date()
+const dataFetched: Ref<boolean> = ref(false)
+const orders: Ref<TypeOrder[]> = ref([])
 
 /* Computed */
-
-const allOrders: ComputedRef<TypeOrder[] | null> = computed(() => {
-    return ordersStore.allOrders
-})
-
-const sortedRoutineTasks: ComputedRef<TypeOrder[] | null> = computed(() => {
-    if (!allOrders.value) return null
-    const incompleteToday = allOrders.value.filter(
-        (task) =>
-            new Date(task.fields.lastCompleted).toDateString() !==
-            today.toDateString()
-    )
-    incompleteToday.sort((a, b) => {
-        return a.fields.task.fields.dueByHour - b.fields.task.fields.dueByHour
-    })
-    const completeToday = allOrders.value.filter(
-        (task) =>
-            new Date(task.fields.lastCompleted).toDateString() ===
-            today.toDateString()
-    )
-    return [...incompleteToday, ...completeToday]
-})
-
-const hasSortedRoutineTasks: ComputedRef<boolean> = computed(() => {
-    return !!sortedRoutineTasks.value && sortedRoutineTasks.value.length > 0
-})
-
-const locationsStore = useLocationsStore()
 
 const activeLocationId: ComputedRef<string | undefined> = computed(() => {
     return locationsStore.activeLocation?.sys.id
 })
 
-const { data } = useFetch('/api/contentful/fetch-entries', {
+const shouldFetch: ComputedRef<boolean> = computed(
+    () => !!activeLocationId.value
+)
+
+/* Functions & lifecycle */
+
+const { data, execute } = useFetch('/api/contentful/fetch-entries', {
     params: computed(() => ({
         content_type: 'order',
         'fields.location.sys.id': activeLocationId.value
-    }))
+    })),
+    immediate: false
 })
 
+orders.value = data.value?.items || []
+
 watch(
-    data,
-    (newData) => {
-        if (newData) {
-            // console.log('Daily tasks data updated:', newData)
-            ordersStore.allOrders = newData.items
-            ordersStore.totalOrders = newData.total
+    shouldFetch,
+    (ready) => {
+        if (ready) {
+            execute().then(() => {
+                dataFetched.value = true
+            })
         }
     },
     { immediate: true }
