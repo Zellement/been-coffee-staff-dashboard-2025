16dd92e0e044cf019a40485d39adce27e0b67dc5
Dan Farrow <dan@zellement.com>
2025-08-31 21:29:04 +0100

    add home button to bottom

--- app/components/Molecules/StickyFooter.vue
+++ app/components/Molecules/StickyFooter.vue
@@ -2,6 +2,9 @@
     <div
         class="dark:from-navy-600 dark:to-navy-500 fixed bottom-0 left-0 z-10 flex w-full items-center justify-between bg-gradient-to-b from-neutral-600 to-neutral-300 p-1"
     >
+        <nuxt-link to="/">
+            <u-icon name="i-bx-home" />
+        </nuxt-link>
         <span class="uc-text uc-text--xs">
             {{ shortDateConverter(today) }}
         </span>
