5b8be205c4c8dc29ce4917bc094b873ad253009e
Dan Farrow <dan@zellement.com>
2025-09-03 21:11:40 +0100

    sort out favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -114,7 +114,7 @@ export default defineNuxtConfig({
                 {
                     rel: 'icon',
                     type: 'image/x-icon',
-                    href: '/icons/favicon.png'
+                    href: '/favicon.png'
                 }
             ]
         }
