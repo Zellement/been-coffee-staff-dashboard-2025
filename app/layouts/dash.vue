3189b03660af45aeaea3c09cc4bca75ed4e8e7a3
Dan Farrow <dan@zellement.com>
2025-09-08 11:03:32 +0100

    contnue

--- app/layouts/dash.vue
+++ app/layouts/dash.vue
@@ -1,5 +1,17 @@
 <template>
     <div class="layout">
+        <div
+            class="p-default bg-seashell-50 dark:bg-primary-800 sticky top-0 z-10 flex w-full items-center justify-end gap-4"
+        >
+            <u-progress v-model="timer" class="max-w-80" :max="REFRESH_TIMER" />
+            <u-button
+                label="Refresh"
+                variant="outline"
+                size="sm"
+                @click="refreshData"
+            />
+            <colour-mode />
+        </div>
         <main class="pb-24">
             <slot />
         </main>
@@ -10,4 +22,25 @@
 const userStore = useUserStore()
 
 userStore.setUserData()
+
+const REFRESH_TIMER = 600 // seconds
+
+const uiStore = useUiStore()
+const refreshData = async () => {
+    uiStore.refreshing = true
+    await refreshNuxtData()
+    uiStore.refreshing = false
+}
+
+const timer: Ref<number> = ref(0)
+onMounted(() => {
+    setInterval(() => {
+        if (timer.value >= REFRESH_TIMER) {
+            refreshData()
+            timer.value = 0
+        } else {
+            timer.value++
+        }
+    }, 1000) // 1 second
+})
 </script>
--- app/pages/location.vue
+++ app/pages/location.vue
@@ -1,30 +1,14 @@
 <template>
-    <div class="p-default sticky top-0 z-10 flex w-full justify-end">
-        <u-button
-            label="Refresh"
-            variant="outline"
-            size="sm"
-            @click="refreshData"
-        />
-    </div>
     <div class="page grid grid-cols-12 gap-4">
         <daily-tasks class="col-span-full" />
         <routine-tasks class="col-span-full" />
         <upcoming-deliveries class="col-span-full" />
         <reviews-listings class="col-span-full" />
         <standing-orders class="col-span-full" />
-        <location-team class="col-span-full" />
     </div>
 </template>
 
 <script setup lang="ts">
-const uiStore = useUiStore()
-const refreshData = async () => {
-    uiStore.refreshing = true
-    await refreshNuxtData()
-    uiStore.refreshing = false
-}
-
 definePageMeta({
     layout: 'dash'
 })
