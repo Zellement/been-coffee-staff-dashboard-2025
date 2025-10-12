80e96886e39b1fa15523bda43057b324f48da537
Dan Farrow <dan@zellement.com>
2025-10-12 10:18:19 +0100

    start

---
+++ app/pages/recipe/index.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="page grid grid-cols-12 gap-4">recipe</div>
+</template>
+
+<script setup lang="ts">
+useHead({
+    title: 'Recipes - Been Coffee Staff Dashboard'
+})
+</script>
