8bc9c76992143e632d0d09474a1327d34984a29e
Dan Farrow <dan@zellement.com>
2025-10-15 16:40:52 +0100

    add

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,6 +1,19 @@
 <template>
     <div class="page grid grid-cols-12 gap-4">
-        <pre class="fixed inset-0 z-10 bg-red-900 text-white">{{ data }}</pre>
+        <div
+            class="overflow-y fixed inset-0 z-[9999] h-dvh bg-red-900 text-white"
+        >
+            <div class="h-full overflow-y-auto">
+                <div v-for="item in shiftsToday?.shifts" :key="item.id">
+                    {{ item.userName }}
+                    <time>{{ new Date(item.start).toLocaleTimeString() }}</time>
+                </div>
+                <pre
+                    v-if="shiftsTomorrow"
+                ><p v-for="item in shiftsTomorrow?.shifts" :key="item.id">{{ item.userName }}</p></pre>
+                <pre class="overflow-y">{{ shiftsToday }}</pre>
+            </div>
+        </div>
         <keep-alive><daily-tasks class="col-span-full" /></keep-alive>
         <keep-alive><routine-tasks class="col-span-full" /></keep-alive>
         <keep-alive><upcoming-deliveries class="col-span-full" /></keep-alive>
@@ -21,8 +34,22 @@ useHead({
     title: 'Been Coffee Staff Dashboard'
 })
 
-const { data } = await useFetch('/api/rotaready/get-shifts', {
-    params: { startDate: '2025-10-21' }
+const { data: shiftsToday } = await useFetch('/api/rotaready/get-shifts', {
+    params: {
+        startDateMin: '2025-10-20',
+        startDateMax: '2025-10-21',
+        endDateMin: '2025-10-20',
+        endDateMax: '2025-10-21'
+    }
+})
+
+const { data: shiftsTomorrow } = await useFetch('/api/rotaready/get-shifts', {
+    params: {
+        startDateMin: '2025-10-21',
+        startDateMax: '2025-10-22',
+        endDateMin: '2025-10-21',
+        endDateMax: '2025-10-22'
+    }
 })
 
 const tableBookingsStore = useTableBookingsStore()
--- server/api/rotaready/get-shifts.ts
+++ server/api/rotaready/get-shifts.ts
@@ -8,19 +8,28 @@ export default defineEventHandler(async (event) => {
     // Build Rotaready query parameters
     const params = new URLSearchParams()
 
-    if (query.startDate) {
+    if (query.startDateMin) {
         // Rotaready expects YYYY-MM-DD format
-        const date = new Date(query.startDate as string)
+        const date = new Date(query.startDateMin as string)
         params.append('startDateMin', String(date.toISOString().split('T')[0]))
     }
 
-    // You could also add optional filters if you want
-    if (query.endDate) {
-        const date = new Date(query.endDate as string)
+    if (query.startDateMax) {
+        const date = new Date(query.startDateMax as string)
         params.append('startDateMax', String(date.toISOString().split('T')[0]))
     }
 
-    const url = `https://api.rotaready.com/rota/shift/paginated?${params.toString()}`
+    if (query.endDateMin) {
+        const date = new Date(query.endDateMin as string)
+        params.append('endDateMin', String(date.toISOString().split('T')[0]))
+    }
+
+    if (query.endDateMax) {
+        const date = new Date(query.endDateMax as string)
+        params.append('endDateMax', String(date.toISOString().split('T')[0]))
+    }
+
+    const url = `https://api.rotaready.com/rota/shift/paginated?${params.toString()}&limit=100`
     console.log('Fetching Rotaready shifts:', url)
 
     const res = await fetch(url, {
