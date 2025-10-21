766a7e19853711857b7e3052bf8746508f149756
Dan Farrow <dan@zellement.com>
2025-10-21 08:32:48 +0100

    remove lazy from shifts

--- app/pages/location.vue
+++ app/pages/location.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="page grid grid-cols-12 gap-4">
-        <lazy-shifts-carousel
+        <shifts-carousel
             class="col-span-full"
             item-classes="md:basis-1/2"
             hide-dots
