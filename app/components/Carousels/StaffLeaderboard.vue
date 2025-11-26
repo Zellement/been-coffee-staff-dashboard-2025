285f30ef39d6206b3749e53dde8cfbb95752f8a8
Dan Farrow <dan@zellement.com>
2025-11-26 22:32:52 +0000

    continue

--- app/components/Carousels/StaffLeaderboard.vue
+++ app/components/Carousels/StaffLeaderboard.vue
@@ -4,27 +4,34 @@
             :title="`From ${periodStartString} until the end of this month`"
         />
         <div class="grid grid-cols-3 gap-4">
-            <div
-                v-for="item in topThreeEmployees"
-                :key="item.sys.id"
-                class="flex flex-col items-center gap-2"
-            >
-                <img
-                    v-if="item.fields.photo && item.fields.photo.length > 0"
-                    class="bg-tertiary size-16 shrink-0 rounded-full p-px"
-                    :src="`${
-                        item.fields.photo[0].fields.file.url
-                    }?w=90&h=90&fit=fill&f=face&fm=webp`"
-                    :alt="`${item.fields.name} ${item.fields.surname}`"
-                />
-                <p class="flex flex-col items-center">
-                    <span>{{ item.fields.name }}</span>
-                    <span>{{ item.fields.surname }}</span>
-                </p>
-                <div class="text-secondary text-center text-sm">
-                    {{ item.thisPeriodMinutes }} minutes logged
+            <template v-for="(item, i) in topThreeEmployees" :key="item.sys.id">
+                <div
+                    :class="getColumnData(i).classes"
+                    class="flex flex-col items-center gap-2 rounded-tl-lg rounded-tr-lg bg-linear-to-b py-4"
+                >
+                    <img
+                        v-if="item.fields.photo && item.fields.photo.length > 0"
+                        class="bg-tertiary relative z-10 size-16 shrink-0 rounded-full p-px"
+                        :src="`${
+                            item.fields.photo[0].fields.file.url
+                        }?w=90&h=90&fit=fill&f=face&fm=webp`"
+                        :alt="`${item.fields.name} ${item.fields.surname}`"
+                    />
+                    <u-icon
+                        v-if="getColumnData(i).icon"
+                        size="40"
+                        :name="getColumnData(i).icon || ''"
+                        class="animate-swing origin-top -translate-y-4"
+                    />
+                    <p class="flex flex-col items-center">
+                        <span>{{ item.fields.name }}</span>
+                        <span>{{ item.fields.surname }}</span>
+                    </p>
+                    <div class="">
+                        {{ item.thisPeriodMinutes }} minutes logged
+                    </div>
                 </div>
-            </div>
+            </template>
         </div>
         <!-- <u-carousel
             v-slot="{ item }"
@@ -72,6 +79,33 @@ const shouldFetch: ComputedRef<boolean> = computed(
 
 /* Functions & lifecycle */
 
+interface ColumnData {
+    classes: string
+    icon?: string
+}
+
+const getColumnData = (index: number): ColumnData => {
+    switch (index) {
+        case 0:
+            return {
+                classes: 'col-start-2 from-yellow-500 to-yellow-700 text-black',
+                icon: 'i-hugeicons-medal-first-place'
+            }
+        case 1:
+            return {
+                classes: 'col-start-1 from-slate-500 to-slate-700 text-white',
+                icon: 'i-hugeicons-medal-second-place'
+            }
+        case 2:
+            return {
+                classes: 'col-start-3 from-yellow-800 to-yellow-900 text-white',
+                icon: 'i-hugeicons-medal-third-place'
+            }
+        default:
+            return { classes: '', icon: 'i-hugeicons-medal-third-place' }
+    }
+}
+
 const { data } = useFetch('/api/contentful/fetch-entries', {
     key: 'staffLeaderboard',
     lazy: true,
@@ -93,3 +127,21 @@ watch(data, (newData) => {
     }
 })
 </script>
+
+<style scoped>
+@keyframes swing {
+    0% {
+        transform: rotate3d(0, 0, 1, -10deg);
+    }
+    50% {
+        transform: rotate3d(0, 0, 1, 10deg);
+    }
+    100% {
+        transform: rotate3d(0, 0, 1, -10deg);
+    }
+}
+
+.animate-swing {
+    animation: swing 3s ease-in-out infinite;
+}
+</style>
