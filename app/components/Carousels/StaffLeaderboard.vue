7627346b57d81e69fe1bff02c03019dc9bda7de1
Dan Farrow <dan@zellement.com>
2025-11-29 10:14:17 +0000

    continiue

--- app/components/Carousels/StaffLeaderboard.vue
+++ app/components/Carousels/StaffLeaderboard.vue
@@ -1,17 +1,18 @@
 <template>
-    <div class="p-c-default">
-        <carousel-title-and-action
-            :title="`From ${periodStartString} until the end of this month`"
-        />
-        <div class="grid grid-cols-3 gap-4">
+    <div class="p-default">
+        <carousel-title-and-action :title="`Task Leaderboard`" />
+        <h3 class="uc-text uc-text--2xs">
+            From {{ periodStartString }} until the end of this month
+        </h3>
+        <div class="grid grid-cols-3 grid-rows-10 gap-6">
             <template v-for="(item, i) in topThreeEmployees" :key="item.sys.id">
                 <div
                     :class="getColumnData(i).classes"
-                    class="flex flex-col items-center gap-2 rounded-tl-lg rounded-tr-lg bg-linear-to-b py-4"
+                    class="mt-8 flex flex-col items-center gap-2 rounded-tl-2xl rounded-tr-2xl bg-linear-to-b pb-4"
                 >
                     <img
                         v-if="item.fields.photo && item.fields.photo.length > 0"
-                        class="bg-tertiary relative z-10 size-16 shrink-0 rounded-full p-px"
+                        class="hexagon-clip relative z-10 -mb-8 size-16 shrink-0 -translate-y-1/2 rounded-full"
                         :src="`${
                             item.fields.photo[0].fields.file.url
                         }?w=90&h=90&fit=fill&f=face&fm=webp`"
@@ -19,17 +20,23 @@
                     />
                     <u-icon
                         v-if="getColumnData(i).icon"
-                        size="40"
+                        :size="getColumnData(i).iconSize || '40'"
                         :name="getColumnData(i).icon || ''"
-                        class="animate-swing origin-top -translate-y-4"
+                        class="animate-swing -mb-5 size-12 origin-top -translate-y-5"
+                        :class="getColumnData(i).iconClasses"
                     />
                     <p class="flex flex-col items-center">
                         <span>{{ item.fields.name }}</span>
-                        <span>{{ item.fields.surname }}</span>
                     </p>
-                    <div class="">
-                        {{ item.thisPeriodMinutes }} minutes logged
-                    </div>
+                    <p class="uc-text text-xl leading-none">
+                        {{ item.thisPeriodMinutes }}
+                    </p>
+                    <p
+                        class="uc-text uc-text--2xs -translate-y-1 text-center leading-none"
+                    >
+                        minutes<br />
+                        logged
+                    </p>
                 </div>
             </template>
         </div>
@@ -82,24 +89,35 @@ const shouldFetch: ComputedRef<boolean> = computed(
 interface ColumnData {
     classes: string
     icon?: string
+    iconSize?: string
+    iconClasses?: string
 }
 
 const getColumnData = (index: number): ColumnData => {
     switch (index) {
         case 0:
             return {
-                classes: 'col-start-2 from-yellow-500 to-yellow-700 text-black',
-                icon: 'i-hugeicons-medal-first-place'
+                classes:
+                    'col-start-2 row-span-full row-start-1 from-yellow-600 to-yellow-500 text-white',
+                icon: 'i-hugeicons-medal-first-place',
+                iconSize: '48',
+                iconClasses: 'text-yellow-300'
             }
         case 1:
             return {
-                classes: 'col-start-1 from-slate-500 to-slate-700 text-white',
-                icon: 'i-hugeicons-medal-second-place'
+                classes:
+                    'col-start-1 row-span-8 row-start-3 from-gray-300 to-gray-500 text-white',
+                icon: 'i-hugeicons-medal-second-place',
+                iconSize: '40',
+                iconClasses: 'text-slate-400'
             }
         case 2:
             return {
-                classes: 'col-start-3 from-yellow-800 to-yellow-900 text-white',
-                icon: 'i-hugeicons-medal-third-place'
+                classes:
+                    'col-start-3 row-span-6 row-start-5 from-yellow-800 to-yellow-900 text-white',
+                icon: 'i-hugeicons-medal-third-place',
+                iconSize: '32',
+                iconClasses: 'text-yellow-700'
             }
         default:
             return { classes: '', icon: 'i-hugeicons-medal-third-place' }
