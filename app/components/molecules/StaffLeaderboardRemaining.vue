d992aa2fef0bf77cd7c805ebf15b3b59ffcc1213
Dan Farrow <dan@zellement.com>
2025-12-28 08:35:12 +0000

    add for remaining

--- app/components/molecules/StaffLeaderboardRemaining.vue
+++ app/components/molecules/StaffLeaderboardRemaining.vue
@@ -6,14 +6,45 @@
                     <p class="text-xl opacity-10">
                         {{ padNumber(i + 4) }}
                     </p>
-                    <img
-                        v-if="item.fields.photo && item.fields.photo.length > 0"
-                        class="hexagon-clip relative z-10 -mb-8 size-10 shrink-0 -translate-y-1/2 rounded-full"
-                        :src="`${
-                            item.fields.photo[0].fields.file.url
-                        }?w=90&h=90&fit=fill&f=face&fm=webp`"
-                        :alt="`${item.fields.name} ${item.fields.surname}`"
-                    />
+                    <u-drawer>
+                        <button>
+                            <img
+                                v-if="
+                                    item.fields.photo &&
+                                    item.fields.photo.length > 0
+                                "
+                                class="hexagon-clip relative z-10 -mb-8 size-10 shrink-0 -translate-y-1/2 rounded-full"
+                                :src="`${
+                                    item.fields.photo[0].fields.file.url
+                                }?w=90&h=90&fit=fill&f=face&fm=webp`"
+                                :alt="`${item.fields.name} ${item.fields.surname}`"
+                            />
+                        </button>
+
+                        <template #content>
+                            <div class="h-full overflow-y-scroll">
+                                <ul class="flex flex-row flex-wrap gap-3 p-2">
+                                    <li
+                                        v-for="task in item?.thisPeriodTasks"
+                                        :key="task.taskName"
+                                        class="flex"
+                                    >
+                                        <u-avatar-group>
+                                            <u-badge color="neutral">{{
+                                                task.taskName
+                                            }}</u-badge>
+                                            <u-badge
+                                                :label="`${fullDateConverter(task.date)} @ ${extractHourAndMinute(
+                                                    task.date
+                                                )}`"
+                                                color="tertiary"
+                                            />
+                                        </u-avatar-group>
+                                    </li>
+                                </ul>
+                            </div>
+                        </template>
+                    </u-drawer>
                     <p class="flex flex-col items-center">
                         <span>{{ item.fields.name }}</span>
                     </p>
@@ -34,6 +65,7 @@
 
 <script lang="ts" setup>
 const staffLeaderboardStore = useStaffLeaderboardStore()
+const { fullDateConverter, extractHourAndMinute } = useDateUtils()
 
 const employeeLeaderboard: ComputedRef<TypeEmployee[]> = computed(() => {
     return staffLeaderboardStore.employeeLeaderboard || []
